a10aa0ed1f47dabc5937cc0df08eb30d34d99db9
reaganforonda <foronda.reagan@gmail.com>
2018-06-18 21:51:52 -0600

    added login route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing';
+import Login from './Components/Login';
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
+        <Route path='/login' component={Login}/>
     </Switch>
 )
