a5494e59e002b12ec7f1e35bade7718e735080f4
reaganforonda <foronda.reagan@gmail.com>
2018-06-18 19:53:36 -0600

    added default font

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Landing extends React.Component{
     render(){
         return (
-            <div>
-                Landing
+            <div className='landing-container'>
+                
             </div>
         )
     }
