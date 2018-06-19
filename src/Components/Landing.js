c11c84d6569158d25bf71ce28ba69d32fababe7b
reaganforonda <foronda.reagan@gmail.com>
2018-06-18 21:26:52 -0600

    landing page styling

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,7 +4,10 @@ export default class Landing extends React.Component{
     render(){
         return (
             <div className='landing-container'>
-                
+                <div className='landing-buttons'>
+                    <button>Sign In</button>
+                    <button>Sign Up</button>
+                </div>
             </div>
         )
     }
