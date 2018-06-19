f23894cb0f25cdd61c024124dd0d9ae5a4998172
reaganforonda <foronda.reagan@gmail.com>
2018-06-18 21:53:18 -0600

    added login page

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Login extends React.Component{
+    render(){
+        return (
+            <div className='login-page-container'>
+                Login Page
+            </div>
+        )
+    }
+}
