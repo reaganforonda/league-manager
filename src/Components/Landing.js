e5dc532304ab934577ac9715532128fafd5f9309
reaganforonda <foronda.reagan@gmail.com>
2018-06-18 21:52:27 -0600

    added buttons to landing

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Landing extends React.Component{
     render(){
         return (
             <div className='landing-container'>
                 <div className='landing-buttons'>
-                    <button>Sign In</button>
-                    <button>Sign Up</button>
+                    <Link to='/login'><button>Sign In</button></Link>
+                    <Link to='/'><button>Sign Up</button></Link>
                 </div>
             </div>
         )
