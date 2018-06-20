1c61895a1805f875bce0b983b407a9d6faa500f6
reaganforonda <foronda.reagan@gmail.com>
2018-06-19 23:32:25 -0600

    added to schema

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -1,18 +1,14 @@
-CREATE TABLE league(
+CREATE TABLE leagues (
     league_id SERIAL PRIMARY KEY,
     league_name VARCHAR(45)
-)
+),
 
-
-CREATE TABLE users{
-    id SERIAL PRIMARY KEY,
-
-}
-
-
-CREATE TABLE teams {
+CREATE TABLE teams(
     team_id SERIAL PRIMARY KEY,
-    league_id INTEGER REFERENCES league(league_id)
-    team_name VARCHAR(45)
-}
+    league_id REFERENCES leagues(league_id),
+    team_name VARCHAR(45),
+    team_city VARCHAR(45),
+    team_state VARCHAR(2),
+    team_zip INTEGER
+)
 
