980d9ea4a47213e4b0e65b4c9a148b5767184297
reaganforonda <foronda.reagan@gmail.com>
2018-06-19 21:43:10 -0600

    added login inputs and button

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,10 +5,12 @@ export default class Login extends React.Component{
     render(){
         return (
             <div className='login-page-container'>
-            <div className='login-page-overlay'>
                 <div className='login-container'>
-
-                </div>
+                    <h2>Welcome to League Manager</h2>
+                    <div className='row-error-message'></div>
+                    <input placeholder='User Name'/>
+                    <input placeholder='Password'/>
+                    <button>Sign In</button>
                 </div>
             </div>
         )
