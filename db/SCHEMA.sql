520f44fe8574d48ec37cf34f493e7c48949bbc50
reaganforonda <foronda.reagan@gmail.com>
2018-06-20 10:33:18 -0600

    added to schema

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -10,5 +10,45 @@ CREATE TABLE teams(
     team_city VARCHAR(45),
     team_state VARCHAR(2),
     team_zip INTEGER
-)
+),
 
+CREATE TABLE seasons(
+    season_id SERIAL PRIMARY KEY,
+    league_id REFERENCES leagues(league_id)
+    season_start_date DATE,
+    season_end_date DATE
+),
+
+CREATE TABLE coaches(
+    coaches_id SERIAL PRIMARY KEY,
+    team_id REFERENCES teams(team_id),
+    league_id REFERENCES leagues(league_id),
+    coach_first_name VARCHAR(45),
+    coach_last_name VARCHAR(45),
+    coach_address VARCHAR(45),
+    coach_city VARCHAR(45),
+    coach_state VARCHAR(2),
+    coach_phone VARCHAR(45),
+    coach_email TEXT,
+    coach_zip INTEGER
+),
+
+CREATE TABLE stadiums(
+    stadium_id SERIAL PRIMARY KEY,
+    stadium_name VARCHAR(45),
+    stadium_address VARCHAR(45),
+    stadium_city VARCHAR(45),
+    stadium_state VARCHAR(45),
+    stadum_zip INTEGER
+),
+
+CREATE TABLE players (
+    player_id SERIAL PRIMARY KEY,
+    p_first_name VARCHAR(45),
+    p_last_name VARCHAR(45),
+    p_address VARCHAR(45),
+    p_city VARCHAR(45),
+    p_state VARCHAR(2),
+    p_phone VARCHAR(45),
+    p_zip INTEGER
+)
