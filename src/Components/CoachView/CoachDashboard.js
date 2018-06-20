25bb44895f1b35d6ff3487e5b0da9c8dc078d3fc
reaganforonda <foronda.reagan@gmail.com>
2018-06-19 21:48:31 -0600

    added coaches dashboard view and route

---
+++ src/Components/CoachView/CoachDashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class CoachDashbaord extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state=[]
+    }
+    
+    render(){
+        return (
+            <div>
+                Dashboard
+            </div>
+        )
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing';
 import Login from './Components/Login';
+import CoachDashboard from './Components/CoachView/CoachDashboard'
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
         <Route path='/login' component={Login}/>
+        <Route path='/coach/dashboard' component={CoachDashboard}/>
     </Switch>
 )
