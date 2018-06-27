44f970e1b20538fbc5bc5ff2db7fc00dfba8718a
reaganforonda <foronda.reagan@gmail.com>
2018-06-27 16:39:39 -0600

    added route

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing';
 import Login from './Components/Login';
-import CoachDashboard from './Components/CoachView/CoachDashboard'
-import Register from './Components/Register/Register'
+import CoachDashboard from './Components/CoachView/CoachDashboard';
+import Register from './Components/Register/Register';
+import RegisterLeague from './Components/Register/RegisterLeague';
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
+        <Route path='/registerleague' component={RegisterLeague}/>
         <Route path='/coach/dashboard' component={CoachDashboard}/>
     </Switch>
 )
