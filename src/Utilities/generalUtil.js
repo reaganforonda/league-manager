7021249b2ac39d40a47a2748e27a0ca38f343d6d
reaganforonda <foronda.reagan@gmail.com>
2018-06-27 08:51:42 -0600

    added email validation util function

--- src/Utilities/generalUtil.js
+++ src/Utilities/generalUtil.js
@@ -1,3 +1,10 @@
 module.exports= {
-    
+    /*
+     * Function to validate email
+     * @param String email = Email that user has entered and needs validation
+     */
+    validateEmail(email) {
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 }
