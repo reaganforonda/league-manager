733edf62f5fbbcce3c34f4b972cdd4160bd43165
reaganforonda <foronda.reagan@gmail.com>
2018-06-26 20:23:02 -0600

    league reducer boilderplate

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
+import leagueReducer from './ducks/reducers/leagueReducer';
 
 const reducers = {
-    
+    leagueReducer : leagueReducer
 }
 
 let middleware = promiseMiddleware();
