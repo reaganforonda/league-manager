450c6d349d38c73070c78ee701e9d43ce7af88fd
reaganforonda <foronda.reagan@gmail.com>
2018-06-26 18:11:39 -0600

    work on login view logic and styling

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,15 +2,39 @@ import React from 'react';
 
 
 export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            userName:'',
+            pw:''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handelLoginSubmit = this.handelLoginSubmit.bind(this);
+    }
+
+    handleInputChange(e){
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handelLoginSubmit(e){
+        e.preventDefault();
+    }
+
     render(){
         return (
             <div className='login-page-container'>
                 <div className='login-container'>
                     <h2>Welcome to League Manager</h2>
                     <div className='row-error-message'></div>
-                    <input placeholder='User Name'/>
-                    <input placeholder='Password'/>
-                    <button>Sign In</button>
+                    <form className='login-form'>
+                        <input onChange={(e)=> this.handleInputChange(e)} 
+                            name='userName' placeholder='User Name'/>
+                        <input onChange={(e)=> this.handleInputChange(e)} 
+                            name='pw' type='password' placeholder='Password'/>
+                        <input type='submit' placeholder='Sign In'/>
+                    </form>
                 </div>
             </div>
         )
