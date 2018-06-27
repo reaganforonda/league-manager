c7c173523777bef65dd188b8abfc97ee87421e17
reaganforonda <foronda.reagan@gmail.com>
2018-06-27 08:48:51 -0600

    fixed bug

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,22 @@ const axios = require('axios');
 
 module.exports = {
     register: async (req, res, next) => {
+        const db = req.app.get('db');
+        const {userType, userName, userEmail, pw } = req.body;
+        console.log(req.body);
 
+        await db.GET_USERNAMES([userName]).then(users=> {
+            if(users.length !== 0){
+                res.status(400).send('Username Taken')
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(pw, salt);
+
+                db.REGISTER_USER([userName, userEmail, hash, userType]).then(user=> {
+                    res.status(200).send('User Created');
+                })
+            }
+        })
     },
 
     login: (req, res, next) => {
@@ -11,10 +26,15 @@ module.exports = {
         const {userName, pw} = req.body;
 
         db.GET_USERNAMES([userName]).then(user => {
-            if(user.length !== 0){
+            
+            // Login if user account type is League
+            if(user.length !== 0 && user[0].acct_type === 1){
+                const userID = user[0].user_id
                 const userPW = user[0].user_pw;
-                
-                const validPassword = bcrypt.compareSync(pw, )
+                const confirmedPW = bcrypt.compareSync(pw);
+                if(confirmedPW){
+                    
+                }
             }
         })
     },
@@ -24,6 +44,6 @@ module.exports = {
     },
 
     validate: (req, res, next) => {
-        
+
     }
 }
