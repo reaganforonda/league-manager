e3f936cc5f256bc9ee878e88f0bee439d5eb1cce
reaganforonda <foronda.reagan@gmail.com>
2018-06-26 20:10:32 -0600

    added to register view

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -7,6 +7,8 @@ export default class Register extends React.Component{
 
         this.state={
             userType: '',
+            leagueName: '',
+            teamName: '',
             userName: '',
             pw: '',
             confirmPW:''
@@ -14,6 +16,7 @@ export default class Register extends React.Component{
 
         this.handleUserTypeChange = this.handleUserTypeChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
 
     handleUserTypeChange(e) {
@@ -22,6 +25,10 @@ export default class Register extends React.Component{
 
     handleInputChange(e){
         this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSubmitForm(e){
+        e.preventDefault();
     }
 
     render(){
@@ -41,15 +48,22 @@ export default class Register extends React.Component{
                         <div className='radio-row'>
                             <input id='team-mng' checked={this.state.userType==='team'} 
                                 onChange={(e)=> this.handleUserTypeChange(e)} value='team' type='radio'/>
-                            <lable htmlFor='team-mng'>Team Manager</lable>
+                            <label htmlFor='team-mng'>Team Manager</label>
                         </div>
                     </div>
                     {
-                        this.state.userType === 'league-mng' ? (
+                        this.state.userType === 'league' ? (
                             <div className='registration-form-row'>
-                                League Name
+                                <input type='text' onChange={(e)=> this.handleInputChange(e)} 
+                                    name='leagueName' placeholder='League Name'/>
                             </div>
-                        ): null
+                            
+                        ): (
+                            <div className='registration-form-row'>
+                                <input type='text' onChange={(e)=> this.handleInputChange(e)} 
+                                    name='teamName' placeholder='Team Name'/>
+                            </div>
+                        )
                     }
                     <div className='registration-form-row'>
                         <input onChange={(e) => this.handleInputChange(e)} name='userName' 
@@ -62,7 +76,10 @@ export default class Register extends React.Component{
                     <div className='registration-form-row'>
                         <input onChange={(e)=> this.handleInputChange(e)} name='confirmPW' 
                             placeholder='Confirm Password'/>
-                    </div>  
+                    </div>
+                    <div className='registration-form-row'>
+                        <input onClick={(e)=>this.handleSubmitForm(e)} type='submit' placeholder='Register'/>
+                    </div>
                 </form>
             </div>
         )
