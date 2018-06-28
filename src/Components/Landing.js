d6ad800831d314596ca5e541c9bd2a620f3c677a
reaganforonda <foronda.reagan@gmail.com>
2018-06-28 08:52:15 -0600

    added link route to sign up

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,7 +7,7 @@ export default class Landing extends React.Component{
             <div className='landing-container'>
                 <div className='landing-buttons'>
                     <Link to='/login'><button>Sign In</button></Link>
-                    <Link to='/'><button>Sign Up</button></Link>
+                    <Link to='/register'><button>Sign Up</button></Link>
                 </div>
             </div>
         )
