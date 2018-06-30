c3320d94802d8b4754a626e1f845dee30f030608
reaganforonda <foronda.reagan@gmail.com>
2018-06-29 20:36:55 -0600

    Started adding components

--- src/Components/LeagueView/LeagueDashboard.js
+++ src/Components/LeagueView/LeagueDashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
-
+import Header from './LeagueHeader'
+import LeagueSideMenu from './LeageMenus/LeagueSideMenu'
 
 export class LeagueDashboard extends React.Component{
     render(){
         return (
             <div className='league-dashboard-container'>
+                <Header/>
+                <LeagueSideMenu/>
                 League Dashboard Container
             </div>
         )
