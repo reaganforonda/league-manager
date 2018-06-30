ec5205b4cfbad7714a6da91da7b1c53bb610d0be
reaganforonda <foronda.reagan@gmail.com>
2018-06-29 20:38:24 -0600

    created components

---
+++ src/Components/LeagueView/LeageMenus/LeagueSideMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class LeagueSideMenu extends React.Component{
+    render(){
+        return (
+            <div className='league-side-menu-container'>
+                <ol className='league-side-menu-list'>
+                    <li className='league-side-menu-item'>Home</li>
+                    <li className='league-side-menu-item'>League</li>
+                </ol>
+            </div>
+        )
+    }
+}
