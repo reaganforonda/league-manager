edcca996f12133e975b463df1d50ab525abf9866
reaganforonda <foronda.reagan@gmail.com>
2018-07-16 08:38:40 -0600

    fixed bugs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const axios = require('axios');
 const generalUtil = require('../../src/Utilities/generalUtil')
 
---
+++ src/Components/CoachView/CoachTeam/CoachTeamCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+export default class CoachTeamCard extends React.Component{
+    render(){
+        return (
+            <div className='coach-team-card-container'>
+
+            </div>
+        )
+    }
+}
