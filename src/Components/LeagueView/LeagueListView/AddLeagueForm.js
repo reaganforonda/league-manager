6963e9f3072a6d349881b657344ccabca08176a1
reaganforonda <foronda.reagan@gmail.com>
2018-07-17 11:32:16 -0600

    worked on addleagueform

--- src/Components/LeagueView/LeagueListView/AddLeagueForm.js
+++ src/Components/LeagueView/LeagueListView/AddLeagueForm.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import axios from 'axios';
 
-export default class AddLeagueForm extends React.Component{
+import {getManagedLeagues} from '../../../ducks/reducers/leagueReducer'
+
+export class AddLeagueForm extends React.Component{
     constructor(props){
         super(props);
 
@@ -9,32 +14,100 @@ export default class AddLeagueForm extends React.Component{
             city: '',
             state:'',
             zipcode:'',
+            displaySuccessRow: false,
+            validForm: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.resetState = this.resetState.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.validateAcctType = this.validateAcctType.bind(this);
     }
+
 
     resetState(){
         this.setState({
-
+            leagueName:'',
+            city: '',
+            state:'',
+            zipcode:'',
         })
     }
 
     handleInputChange(e){
         this.setState({[e.target.name]: e.target.value})
     }
+
+    validateAcctType(){
+        if(this.props.user.acct_type === 1 && this.props.user.user_id){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+        if(this.validateAcctType()){
+            let league={
+                user_id: this.props.user.user_id,
+                leagueName: this.state.leagueName,
+                leagueCity: this.state.city,
+                leagueState: this.state.state,
+                leagueZipcode: this.state.zipcode
+            }
+
+            axios.post('/api/register/league',league).then((league)=> {
+                this.props.getManagedLeagues(this.props.user.user_id);
+                this.setState({displaySuccessRow: true})
+                this.resetState();
+            }).catch((err)=>{
+                console.log(`Error while attempting to add league: ${league}`)
+            })
+        } else {
+            // TODO: ADD SOMETHING
+        }
+    }
+
     render(){
         return (
             <div className='add-league-form-container'>
                 <form className='add-league-form'>
-                    <input type='text' name='leagueName' placeholder='League Name'/>
-                    <input type='text' name='city' placeholder='City'/>
-                    <input type='text' name='state' placeholder='State'/>
-                    <input type='number' name='zipcode' placeholder='Zipcode'/>
-                    <input type='submit' placeholder='Submit'/>
+                    <div className='add-league-form-row'>
+                        <input required type='text' name='leagueName' placeholder='League Name' 
+                            onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='add-league-form-row'>
+                        <input required type='text' name='city' placeholder='City' 
+                            onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='add-league-form-row'>
+                        <input required type='text' name='state' placeholder='State' 
+                            onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='add-league-form-row'>
+                        <input required type='number' name='zipcode' placeholder='Zipcode' 
+                            onChange={(e)=>this.handleInputChange(e)}/>
+                    </div>
+                    <div className='add-league-form-row'>
+                        <input type='submit' placeholder='Submit' onClick={(e)=>this.handleSubmit(e)}/>
+                    </div>
                 </form>
+                {
+                    this.state.displaySuccessRow ? (
+                        <div className='add-league-success-row'>League Successfully Added</div>
+                    ):null
+                }
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return {
+        user: state.userReducer.user
+    }
+}
+
+export default connect(mapStateToProps, {getManagedLeagues})(withRouter(AddLeagueForm))
