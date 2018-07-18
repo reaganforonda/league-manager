6e57ba44ebc4682ef11bc450eddf800071907169
reaganforonda <foronda.reagan@gmail.com>
2018-07-18 16:36:16 -0600

    added league name to card

--- src/Components/CoachView/CoachTeam/CoachTeamCard.js
+++ src/Components/CoachView/CoachTeam/CoachTeamCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function CoachTeamCard(props){
         return (
             <div className='coach-team-card-container'>
+                <p>League Name: {props.team.league_name}</p>
                 <p>Team Name: {props.team.team_name}</p>
                 <p>Team City: {props.team.team_city}</p>
                 <p>Approved: {String(props.team.approved)}</p>
