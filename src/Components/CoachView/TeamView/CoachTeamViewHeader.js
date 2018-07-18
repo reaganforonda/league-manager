449a40905508c592243dc9a376c65d483112c346
reaganforonda <foronda.reagan@gmail.com>
2018-07-18 14:22:58 -0600

    added Links

--- src/Components/CoachView/TeamView/CoachTeamViewHeader.js
+++ src/Components/CoachView/TeamView/CoachTeamViewHeader.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 
 export default function CoachTeamViewHeader(props){
     return (
         <div className='coach-team-view-header'>
             <ul className='coach-team-view-header-menu'>
-                <li>Add Team</li>
-                <li>View Teams</li>
+                <li><Link to='/coach/dashboard/teamview/add'>Add Team</Link></li>
+                <li><Link to='/coach/dashboard/teamview/list'>View Teams</Link></li>
             </ul>
         </div>
     )
