2f8b99033776f268db3a8010c2535c7376ac71b5
reaganforonda <foronda.reagan@gmail.com>
2018-07-29 20:51:02 +0100

    added back button

--- src/Components/ErrorViews/Error400View.js
+++ src/Components/ErrorViews/Error400View.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
-export default function Error400View(props){
+export function Error400View(props){
+    function handleOnClick(e){
+        e.preventDefault();
+
+        props.history.push('/#/')
+    }
     return(
         <div className='error-view-container'>
             <h1>Error 400: </h1>
+            <button onClick={(e)=> handleOnClick(e)}>Home Page</button>
         </div>
     )
+
 }
+export default withRouter(Error400View)
--- src/Components/ErrorViews/Error401View.js
+++ src/Components/ErrorViews/Error401View.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
-export default function Error401View(props){
+export function Error401View(props){
+    function handleOnClick(e){
+        e.preventDefault();
+        props.history.push('/#/')
+    }
+
     return(
         <div className='error-view-container'>
             <h1>Error 401: </h1>
+            <button onClick={(e)=> handleOnClick(e)}>Home Page</button>
         </div>
     )
 }
+
+export default withRouter(Error401View)
