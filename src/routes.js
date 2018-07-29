0a9a91f225a72dfb96124ae472b384b4b1db7045
reaganforonda <foronda.reagan@gmail.com>
2018-07-29 20:33:18 +0100

    added error routes

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import Login from './Components/Login';
 import CoachDashboard from './Components/CoachView/CoachDashboard';
 import Register from './Components/Register/Register';
 import LeagueDashboard from './Components/LeagueView/LeagueDashboard';
+import Error500View from './Components/ErrorViews/Error500View';
+import Error400View from './Components/ErrorViews/Error400View';
+import Error401View from './Components/ErrorViews/Error401View';
 
 import RegisterLeague from './Components/Register/RegisterLeague';
 
@@ -17,5 +20,8 @@ export default (
         <Route path='/registerleague' component={RegisterLeague}/>
         <Route path='/coach/dashboard' component={CoachDashboard}/>
         <Route path='/league/dashboard' component={LeagueDashboard}/>
+        <Route path='/error/500' component={Error500View}/>
+        <Route path='/error/401' component={Error401View}/>
+        <Route path='/error/400' component={Error400View}/>
     </Switch>
 )
