5400a0316bfa7121a62bc33d3aec8837ed765dd6
reaganforonda <foronda.reagan@gmail.com>
2018-07-29 16:36:48 +0100

    comment out registerserviceworker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import './assets/styles/main.css'
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 
 ReactDOM.render(<Provider store={store}><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
