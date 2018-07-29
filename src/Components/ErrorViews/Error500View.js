32ca0aa1ee6dfa1ef4202e9cd70cc6e7a009d8f6
reaganforonda <foronda.reagan@gmail.com>
2018-07-29 20:44:19 +0100

    added back button

--- src/Components/ErrorViews/Error500View.js
+++ src/Components/ErrorViews/Error500View.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
-export default function Error500View(props){
+export  function Error500View(props){
+
+    function handleOnClick(e){
+        e.preventDefault();
+        props.history.push('/#')
+    }
+
     return(
         <div className='error-view-container'>
             <h1>Error 500: Server Error</h1>
+            <button onClick={(e)=> handleOnClick(e)}>Home Page</button>
         </div>
     )
 }
+
+export default withRouter(Error500View)
