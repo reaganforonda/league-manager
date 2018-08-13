5e60cafcbaf36e1d2811b16031c8fd69e8115fd0
reaganforonda <foronda.reagan@gmail.com>
2018-08-13 10:49:18 -0600

    rearrange rename

--- src/Components/LeagueManagerView/LeagueManagerDashboard/LeagueDashboard.js
+++ src/Components/LeagueManagerView/LeagueManagerDashboard/LeagueDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {withRouter, Switch, Route} from 'react-router-dom';
 import Header from './LeagueHeader'
 import LeagueSideMenu from './LeageMenus/LeagueSideMenu'
