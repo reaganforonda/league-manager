6277fc206fa6403ea7be3d7c7319fd4837d4365e
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 11:58:40 -0600

    created league table file

---
+++ src/Components/TeamView/LeagueTable.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class LeagueTable extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={}
+    }
+    
+
+    render(){
+        return (
+            <div className='league-table-container'>
+            
+            </div>
+        )
+    }
+}
