d0892fbb442068008422a83487d47ab21bec36f9
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 12:18:02 -0600

    added fixture menu items in header

--- src/Components/FixturesView/FixturesViewHeader.js
+++ src/Components/FixturesView/FixturesViewHeader.js
@@ -3,7 +3,14 @@ import React from 'react';
 export default function FixturesViewHeader(props){
     return (
         <header className='fixture-view-header'>
-            
+            <ul className='fixture-view-header-menu'>
+                <li className='fixture-view-header-menu-item'>
+                    Add Fixture
+                </li>
+                <li className='fixture-view-header-menu-item'>
+                    Update Fixture Stats
+                </li>
+            </ul>
         </header>
     )
 }
