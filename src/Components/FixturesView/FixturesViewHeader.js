5bf17495ac4522bca97524bdae2d00e62366e704
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 12:16:52 -0600

    added fixtures view header

---
+++ src/Components/FixturesView/FixturesViewHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FixturesViewHeader(props){
+    return (
+        <header className='fixture-view-header'>
+            
+        </header>
+    )
+}
