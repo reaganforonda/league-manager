c11f760b26dea2272f791d4465d7fdf13efeb9b0
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 11:41:41 -0600

    added league admin header menu items

--- src/Components/LeagueManagerView/LeagueAdminView/LeagueAdminHeader.js
+++ src/Components/LeagueManagerView/LeagueAdminView/LeagueAdminHeader.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function LeagueAdminHeader(props){
     return (
         <div className='league-admin-header'>
-            League Admin Header
+            <ul className='league-admin-header-menu'>
+                <li className='league-admin-header-menu-item'>
+                    Home
+                </li>
+                <li className='league-admin-header-menu-item'>
+                    Games
+                </li>
+                <li className='league-admin-header-menu-item'>
+                    Seasons
+                </li>
+            </ul>
         </div>
     )
 }
