d9217df8a8f048a16610308c111b26e0cb1132ec
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 15:55:58 -0600

    added seasons view header

---
+++ src/Components/SeasonsView/SeaonsViewHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SeasonsViewHeader(props){
+    return(
+        <header className='seasons-view-header'>
+            <ul className='seasons-view-header-menu'>
+                <li className='seasons-view-header-menu-item'>
+                    Item 1
+                </li>
+                <li className='seasons-view-header-menu-item'>
+                    Item 2
+                </li>
+            </ul>
+        </header>
+    )
+}
