a8aec34c82c722e9acb34bfcfde3024c033eac31
reaganforonda <foronda.reagan@gmail.com>
2018-08-14 15:33:15 -0600

    added fixture form

---
+++ src/Components/FixturesView/AddFixtureForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class AddFixtureForm extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {}
+    }
+
+    
+    render(){
+        return (
+            <form className='add-fixture-form'>
+            
+            </form>
+        )
+    }
+}
