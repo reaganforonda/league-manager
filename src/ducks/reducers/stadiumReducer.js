789e46f103738f9336424b4d8de1af50bf66f585
reaganforonda <foronda.reagan@gmail.com>
2018-08-15 17:30:04 -0600

    added reducer for stadiums

---
+++ src/ducks/reducers/stadiumReducer.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const INITIAL_STATE={
+
+}
+
+
+export default function stadiumReducer(state = INITIAL_STATE, action){
+    switch(action.type){
+        default:
+            return state
+    }
+}
