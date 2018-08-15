485bb4409de0d6fcb9d7409ac78cd5117486d8ba
reaganforonda <foronda.reagan@gmail.com>
2018-08-15 17:30:16 -0600

    connect reducer to store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import promiseMiddleware from 'redux-promise-middleware';
 import leagueReducer from './ducks/reducers/leagueReducer';
 import teamReducer from './ducks/reducers/teamReducer';
 import userReducer from './ducks/reducers/userReducer';
+import stadiumReducer from './ducks/reducers/stadiumReducer'
 
 const reducers = {
     leagueReducer : leagueReducer,
     teamReducer : teamReducer,
-    userReducer : userReducer
+    userReducer : userReducer,
+    stadiumReducer: stadiumReducer
 }
 
 let middleware = promiseMiddleware();
