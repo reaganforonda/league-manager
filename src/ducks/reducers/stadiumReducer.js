3722b60f0a5200eb8a00e8430244f37524410886
reaganforonda <foronda.reagan@gmail.com>
2018-08-15 23:50:10 -0600

    stadium reducer template

--- src/ducks/reducers/stadiumReducer.js
+++ src/ducks/reducers/stadiumReducer.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const INITIAL_STATE={
-
+    allStadiums : [],
+    selectedStadium: []
 }
 
+const GET_ALL_STADIUMS = 'GET_ALL_STADIUMS';
+const LOAD_STADIUM_INFO = 'LOAD_STADIUM_INFO';
 
 export default function stadiumReducer(state = INITIAL_STATE, action){
     switch(action.type){
