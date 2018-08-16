c6552e3f7d27664ac3419517bb34aa72955216d6
reaganforonda <foronda.reagan@gmail.com>
2018-08-15 23:23:01 -0600

    add function to add new stadium

--- server/controllers/stadiumController.js
+++ server/controllers/stadiumController.js
@@ -3,6 +3,24 @@ const axios = require('axios');
 module.exports = {
     addStadium: (req, res) => {
         const db = req.app.get('db');
-        const {stadium}
-    }    
+        const {
+            user,
+            stadium_name,
+            stadium_address,
+            stadium_city,
+            stadium_state,
+            stadium_zip
+        } = req.body;
+
+        if (user.acct_type === 1) {
+            db.CREATE_STADIUM([stadium_name, stadium_address, stadium_city, stadium_state, stadium_zip]).then((result) => {
+                res.status(200).send(result);
+            }).catch((err) => {
+                console.log(`Server error while attempting to add new stadium: ${err}`)
+                res.sendStatus(500);
+            })
+        } else {
+            res.sendStatus(401);
+        }
+    }
 }
