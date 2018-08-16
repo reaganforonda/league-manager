0da082f1e5be1c3f5bbf9e3c3e988a82f60e60f8
reaganforonda <foronda.reagan@gmail.com>
2018-08-15 21:54:26 -0600

    work add stadium form functionality

--- src/Components/StadiumView/AddStadiumForm.js
+++ src/Components/StadiumView/AddStadiumForm.js
@@ -6,16 +6,75 @@ export class AddStadiumForm extends React.Component{
     constructor(props){
         super(props);
 
-        this.state={};
+        this.state={
+            locationName: '',
+            locationAddress: '',
+            locationState: '',
+            locationCity: '',
+            locationZip: ''
+        };
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.resetForm = this.resetForm.bind(this);
     }
+
+    handleInputChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit(e){
+        
+        e.preventDefault();
+
+        let location = {
+            stadium_name : this.state.locationName,
+            stadium_address: this.state.locationAddress,
+            stadium_city: this.state.locationCity,
+            stadium_state : this.state.locationState,
+            stadium_zip: this.state.locationZip
+        }
+
+        console.log(location);
+        this.resetForm();
+    }
+
+    resetForm(){
+        this.setState({
+            locationName: '',
+            locationAddress: '',
+            locationState: '',
+            locationCity: '',
+            locationZip: ''
+        })
+    }
+
     render(){
         return (
             <form className='add-stadium-form'>
                 <div className='add-stadium-form-row'>
-                    <input placeholder='Location Name'/>
+                    <input type='text' value={this.state.locationName} 
+                        onChange={(e)=>this.handleInputChange(e)} name='locationName' placeholder='Location Name'/>
                 </div>
                 <div className='add-stadium-form-row'>
-                    <input placeholder='Location Name'/>
+                    <input type='text' value={this.state.locationAddress} 
+                        onChange={(e)=>this.handleInputChange(e)} name='locationAddress' placeholder='Address'/>
+                </div>
+                <div className='add-stadium-form-row'>
+                    <input type='text' value={this.state.locationCity} 
+                        onChange={(e)=>this.handleInputChange(e)} name='locationCity' placeholder='City'/>
+                </div>
+                <div className='add-stadium-form-row'>
+                    <input type='text' value={this.state.locationState} 
+                        onChange={(e)=>this.handleInputChange(e)} name='locationState' placeholder='State'/>
+                </div>
+                <div className='add-stadium-form-row'>
+                    <input type='text' value={this.state.locationZip} 
+                        onChange={(e)=>this.handleInputChange(e)} name='locationZip' placeholder='Zip'/>
+                </div>
+                
+                <div className='add-stadium-form-row'>
+                    <input onClick={(e)=> this.handleSubmit(e)} type='submit' placeholder='Add Location'/>
                 </div>
             </form>
         )
