9c33e57f765692f650c0328baeb613aa8fa8914d
reaganforonda <foronda.reagan@gmail.com>
2018-08-16 13:13:22 -0600

    add get all stadium function in reducer

--- src/ducks/reducers/stadiumReducer.js
+++ src/ducks/reducers/stadiumReducer.js
@@ -8,8 +8,25 @@ const INITIAL_STATE={
 const GET_ALL_STADIUMS = 'GET_ALL_STADIUMS';
 const LOAD_STADIUM_INFO = 'LOAD_STADIUM_INFO';
 
+export function getAllStadiums(leagueID){
+    let stadiums = axios.get(`/api/stadiums/${leagueID}`).then((result) => {
+        return result.data
+    })
+
+    return {
+        type: GET_ALL_STADIUMS,
+        payload: stadiums
+    }
+}
+
 export default function stadiumReducer(state = INITIAL_STATE, action){
     switch(action.type){
+        case GET_ALL_STADIUMS + '_PENDING':
+            return "Loading";
+
+        case GET_ALL_STADIUMS + "_FULFILLED":
+            return Object.assign({}, state, {allStadiums: action.payloead})
+            
         default:
             return state
     }
