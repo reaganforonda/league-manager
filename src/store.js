4ead120fc48bd6fdcf55cb2506b905739ac6bdf1
reaganforonda <foronda.reagan@gmail.com>
2018-08-16 17:59:00 -0600

    added fixturereducer to store

--- src/store.js
+++ src/store.js
@@ -3,13 +3,15 @@ import promiseMiddleware from 'redux-promise-middleware';
 import leagueReducer from './ducks/reducers/leagueReducer';
 import teamReducer from './ducks/reducers/teamReducer';
 import userReducer from './ducks/reducers/userReducer';
-import stadiumReducer from './ducks/reducers/stadiumReducer'
+import stadiumReducer from './ducks/reducers/stadiumReducer';
+import fixtureReducer from './ducks/reducers/fixtureReducer';
 
 const reducers = {
     leagueReducer : leagueReducer,
     teamReducer : teamReducer,
     userReducer : userReducer,
-    stadiumReducer: stadiumReducer
+    stadiumReducer: stadiumReducer,
+    fixtureReducer: fixtureReducer
 }
 
 let middleware = promiseMiddleware();
