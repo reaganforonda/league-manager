bd2894c6793faadd1799e6b4eab401c2866e734c
reaganforonda <foronda.reagan@gmail.com>
2018-08-17 19:50:57 -0600

    remove register league route

--- src/routes.js
+++ src/routes.js
@@ -9,15 +9,11 @@ import Error500View from './Components/ErrorViews/Error500View';
 import Error400View from './Components/ErrorViews/Error400View';
 import Error401View from './Components/ErrorViews/Error401View';
 
-import RegisterLeague from './Components/Register/RegisterLeague';
-
-
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
-        <Route path='/registerleague' component={RegisterLeague}/>
         <Route path='/coach/dashboard' component={CoachDashboard}/>
         <Route path='/league/dashboard' component={LeagueDashboard}/>
         <Route path='/error/500' component={Error500View}/>
