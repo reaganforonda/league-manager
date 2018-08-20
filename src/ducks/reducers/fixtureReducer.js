b507e0c708636c0b8a4256fc8fa377251d78f52f
reaganforonda <foronda.reagan@gmail.com>
2018-08-19 19:24:26 -0600

    added function to retrieve fixtures in reducer

--- src/ducks/reducers/fixtureReducer.js
+++ src/ducks/reducers/fixtureReducer.js
@@ -9,15 +9,25 @@ const GET_CURRENT_UNPLAYED_FIXTURES = 'GET_CURRENT_UNPLAYED_FIXTURES';
 const GET_ALL_CURRENT_FIXTURES = 'GET_ALL_CURRENT_FIXTURES';
 
 export function getAllCurrentFixtures(leagueID, seasonID){
-    let currentFixtures = axios.get(`/api/fixtures/search?q`)
+    let currentFixtures = axios.get(`/api/fixtures/search?leagueID=${leagueID}&seasonID=${seasonID}`).then((result) => {
+        return result.data;
+    })
+
     return {
         type: GET_ALL_CURRENT_FIXTURES,
-        payload: ''
+        payload: currentFixtures
     }
 }
 
 export default function fixtureReducer(state = INITIAL_STATE, action){
     switch(action.type){
+
+        case GET_ALL_CURRENT_FIXTURES + '_PENDING':
+            return "Loading";
+
+        case GET_ALL_CURRENT_FIXTURES + '_FULFILLED':
+            return Object.assign({},state, {allCurrentSeasonFixtures : action.payload});
+
         default:
             return state;
     }
