d0872f98431ecb1ce7c369c18098fb85509223e6
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 12:11:27 -0600

    edit routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
 import Login from './Components/Login/Login';
-import CoachDashboard from './Components/CoachView/CoachDashboard';
 import Register from './Components/Register/Register';
-import LeagueDashboard from './Components/LeagueManagerView/LeagueManagerDashboard/LeagueDashboard';
 import Error500View from './Components/ErrorViews/Error500View';
 import Error400View from './Components/ErrorViews/Error400View';
 import Error401View from './Components/ErrorViews/Error401View';
@@ -14,8 +12,6 @@ export default (
         <Route exact path='/' component={Landing}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
-        <Route path='/coach/dashboard' component={CoachDashboard}/>
-        <Route path='/league/dashboard' component={LeagueDashboard}/>
         <Route path='/error/500' component={Error500View}/>
         <Route path='/error/401' component={Error401View}/>
         <Route path='/error/400' component={Error400View}/>
