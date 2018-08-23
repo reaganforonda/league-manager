24a9afe8011573cd7423470bf92e6bbef2b27cdc
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 13:37:06 -0600

    added Link and paths

--- src/Components/LeagueManager/SideMenu/SideNav.js
+++ src/Components/LeagueManager/SideMenu/SideNav.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function SideNave(props){
     return (
         <div className='league-side-nav'>
-            <ul className='league-side-nav-menu'>
-                <li>Dashboard</li>
-                <li>Leagues</li>
-                <li>Seasons</li>
-                <li>Fixtures</li>
-                <li>Teams</li>
-                <li>Players</li>
-                <li>Stadiums</li>
-            </ul>
+            <div className='league-side-nav-menu'>
+                <Link to='/leaguemanager/dashboard'>Dashboard</Link>
+                <Link to='/leaguemanger/leagueview'>Leagues</Link>
+                <Link to='/leaguemanager/seasons'>Seasons</Link>
+                <Link to='/leaguemanager/fixtures'>Fixtures</Link>
+                <Link to='/leaguemanager/teams'>Teams</Link>
+                <Link to='/leaguemanager/players'>Players</Link>
+                <Link to='/leaguemanager/stadiums'>Stadiums</Link>
+            </div>
         </div>
     )
 }
