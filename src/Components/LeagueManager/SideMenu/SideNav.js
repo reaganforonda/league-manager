26780dda5dc616af7ef7bca1cf9cab7d55a2d57e
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 13:39:54 -0600

    fixed typo

--- src/Components/LeagueManager/SideMenu/SideNav.js
+++ src/Components/LeagueManager/SideMenu/SideNav.js
@@ -6,13 +6,13 @@ export default function SideNave(props){
         <div className='league-side-nav'>
             <div className='league-side-nav-menu'>
                 <Link to='/leaguemanager/dashboard'>Dashboard</Link>
-                <Link to='/leaguemanger/leagueview'>Leagues</Link>
+                <Link to='/leaguemanager/leagueview'>Leagues</Link>
                 <Link to='/leaguemanager/seasons'>Seasons</Link>
                 <Link to='/leaguemanager/fixtures'>Fixtures</Link>
                 <Link to='/leaguemanager/teams'>Teams</Link>
                 <Link to='/leaguemanager/players'>Players</Link>
                 <Link to='/leaguemanager/stadiums'>Stadiums</Link>
-            </div>
+            </div>  
         </div>
     )
 }
