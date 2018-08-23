f6ee33529e3ee0f57c35a229a4d1adde3b6fc02a
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 12:46:17 -0600

    added header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(props){
+    return (
+        <header className='header'>
+            <h1>On the Bounce</h1>
+        </header>
+    )
+}
--- src/Components/LeagueManager/MainView/LeagueManagerMainView.js
+++ src/Components/LeagueManager/MainView/LeagueManagerMainView.js
@@ -2,13 +2,12 @@ import React from 'react';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard'
 import SideNav from '../SideMenu/SideNav';
+import Header from '../../Header/Header';
 
 export default function LeagueManagerMainView(props) {
     return (
         <div className='league-main-view-container'>
-            <div>
-                Header Placeholder
-            </div>
+            <Header/>
             <main className='league-main-view-main'>
                 <SideNav/>
                 <Switch>
