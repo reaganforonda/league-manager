dd8bbcdb6749a13ffe34e017da3ffad928635826
reaganforonda <foronda.reagan@gmail.com>
2018-08-24 14:26:50 -0600

    leaguedropdown

--- src/Components/DropdownMenus/LeagueDropDown.js
+++ src/Components/DropdownMenus/LeagueDropDown.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 export default function LeagueDropDown (props) {
-    console.log(props.leagues);
-
     let leagues = props.leagues.map((value, i) => {
         return <option key={value.league_id + i} value={value.league_id} >{value.league_name}</option>
     })
 
     return (
-        <select className='league-drop-down'>
+        <select onChange={(e)=>props.selectLeague(e)} name='leagueID' className='league-drop-down'>
             <option value="" disabled selected>Select League</option>
             {leagues};
         </select>
