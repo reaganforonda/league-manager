86275019b024a2b2c0ec5e8bb5abf285fddc39c9
reaganforonda <foronda.reagan@gmail.com>
2018-08-24 12:46:03 -0600

    add create player func

--- server/controllers/playerController.js
+++ server/controllers/playerController.js
@@ -1,5 +1,41 @@
 const axios = require('axios');
 
 module.exports = {
-    
+    createPlayer: (req, res, next) => {
+        const db = req.app.get('db');
+        const {
+            leagueID,
+            teamID,
+            firstName,
+            lastName,
+            address,
+            city,
+            state,
+            phone,
+            zip,
+            position,
+            email,
+            active
+        } = req.body;
+
+        db.CREATE_PLAYER([
+            leagueID,
+            teamID,
+            firstName,
+            lastName,
+            address,
+            city,
+            state,
+            phone,
+            zip,
+            email,
+            position,
+            active
+        ]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to create Player: ${err}`);
+            res.sendStatus(500);
+        })
+    },
 }
