1d6bd3083c4e9663ab9596e007dde8ba01a48503
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 19:07:25 -0600

    fix sql for adding league

--- db/CREATE_LEAGUE.sql
+++ db/CREATE_LEAGUE.sql
@@ -1,4 +1,4 @@
 INSERT INTO leagues
-(user_id, league_name, league_city, league_state, league_zip)
+(user_id, league_name, league_city, league_state, league_zip, max_teams, number_games, min_players_per_team, max_players_per_team)
 VALUES
-($1, $2, $3, $4, $5);
+($1, $2, $3, $4, $5, $6, $7, $8, $9)
