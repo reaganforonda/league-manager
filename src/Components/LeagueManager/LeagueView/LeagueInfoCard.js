ce3da6d833d24fa7ffb5b2d3bd6cfc610b28f034
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 22:02:58 -0600

    info card

---
+++ src/Components/LeagueManager/LeagueView/LeagueInfoCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LeagueInfoCard(props){
+    return (
+        <div className='league-info-card-container'>
+            League Name
+        </div>
+    )
+}
