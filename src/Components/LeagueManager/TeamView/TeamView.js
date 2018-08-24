935f51e07caaa1139bbc2ee3c33fc04b048570a1
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 22:19:56 -0600

    add routes

--- src/Components/LeagueManager/TeamView/TeamView.js
+++ src/Components/LeagueManager/TeamView/TeamView.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import TeamViewHeader from './TeamViewHeader';
 
+
 export default function TeamView(props) {
     return (
         <div className='league-team-view-container'>
             <TeamViewHeader/>
             <main className='league-team-view-main'>
                 <Switch>
-
+                    <Route exact path='/leaguemanager/teams' component={}/>
                 </Switch>
             </main>
         </div>
