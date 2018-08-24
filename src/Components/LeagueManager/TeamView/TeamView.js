c9951a5d549379dc286fdaeb3ebb6d2d89ba472a
reaganforonda <foronda.reagan@gmail.com>
2018-08-23 22:25:39 -0600

    added routes

--- src/Components/LeagueManager/TeamView/TeamView.js
+++ src/Components/LeagueManager/TeamView/TeamView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import TeamViewHeader from './TeamViewHeader';
-
+import TeamMain from './TeamMain';
+import TeamAddForm from './TeamAddForm';
 
 export default function TeamView(props) {
     return (
@@ -9,7 +10,8 @@ export default function TeamView(props) {
             <TeamViewHeader/>
             <main className='league-team-view-main'>
                 <Switch>
-                    <Route exact path='/leaguemanager/teams' component={}/>
+                    <Route exact path='/leaguemanager/teams' component={TeamMain}/>
+                    <Route path='/leaguemanager/teams/add' component={TeamAddForm}/>
                 </Switch>
             </main>
         </div>
