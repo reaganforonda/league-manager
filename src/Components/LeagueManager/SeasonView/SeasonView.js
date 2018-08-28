d6834a149157e837ccb82bebd2a13e6102939e1b
reaganforonda <foronda.reagan@gmail.com>
2018-08-28 16:24:32 -0600

    create season view

---
+++ src/Components/LeagueManager/SeasonView/SeasonView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+export default function SeasonView(props) {
+    return (
+        <div className='season-view-container'>
+            <main className='season-view-main'>
+                <Switch>
+
+                    
+                </Switch>
+            </main>
+        </div>
+    )
+}
