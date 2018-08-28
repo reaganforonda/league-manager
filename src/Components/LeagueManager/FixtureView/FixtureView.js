c8ec80b90d8f52cd9ea34dac9b744d109734e8f3
reaganforonda <foronda.reagan@gmail.com>
2018-08-28 16:18:42 -0600

    add routes

--- src/Components/LeagueManager/FixtureView/FixtureView.js
+++ src/Components/LeagueManager/FixtureView/FixtureView.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import FixtureMain from './FixtureMain';
 import FixtureAddForm from './FixtureAddForm';
+import FixtureViewHeader from './FixtureViewHeader';
 
 export default function FixtureView(props) {
     return (
         <div className='fixture-view-container'>
-            
+            <FixtureViewHeader/>
             <main className='fixture-view-main'>
                 <Switch>
-                    <Route exact pather='/leaguemanager/fixtures' component={FixtureMain}/>
+                    <Route exact path='/leaguemanager/fixtures' component={FixtureMain}/>
                     <Route path='/leaguemanager/fixtures/add' component={FixtureAddForm}/>
                 </Switch>
             </main>
