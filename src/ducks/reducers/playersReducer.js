9d552e31c76ae0442a1b7e851f62ac3e60f8682d
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 10:55:54 -0600

    added getallplayerslm

--- src/ducks/reducers/playersReducer.js
+++ src/ducks/reducers/playersReducer.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 
 const INITIAL_STATE = {
-
+    allPlayersLeagueManager: [],
+    leaguePlayersLeagueManager: [],
+    teamPlayersLeagueMaanger: []
 }
 
+const GET_ALL_PLAYERS_LM = "GET_ALL_PLAYERS_LM"
+
+export function getAllPlayersLM(userID) {
+    let players = axios.get(`/api/players?userID=${userID}`).then((result) => {
+        return result
+    })
+
+    return {
+        type: GET_ALL_PLAYERS_LM,
+        payload: players
+    }
+}
 
 export default function playersReducer(state=INITIAL_STATE, action) {
     switch(action.type) {
+        case GET_ALL_PLAYERS_LM + "_PENDING":
+            return "Loading";
+
+        case GET_ALL_PLAYERS_LM + "_FULFILLED":
+            return Object.assign({}, state, {allPlayersLeagueManager : action.payload});
         default:
             return state
     }
