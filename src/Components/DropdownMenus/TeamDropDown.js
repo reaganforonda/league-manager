0112008ba31bc39b501794a29becde33859a6e49
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 09:24:27 -0600

    fix error

--- src/Components/DropdownMenus/TeamDropDown.js
+++ src/Components/DropdownMenus/TeamDropDown.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function TeamDropDown (props){
     let teams = props.teams.map((value, i) => {
-        return <option key={value.team_id + } value={value.team_id}> {value.team_id} </option>
+        return <option key={value.team_id + i} value={value.team_id}> {value.team_name} </option>
     });
 
 
     return (
-        <select onChange={(e) => prop.selectTeam(e)} name='teamID' className='team-drop-down'>
+        <select onChange={(e) => props.selectTeam(e)} name='teamID' className='team-drop-down'>
             <option value='' disabled selected>Select Team</option>
             {teams}
         </select>
