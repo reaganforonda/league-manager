1f38d982506ed34aa09575e53e414ccc7dc10f2f
reaganforonda <foronda.reagan@gmail.com>
2018-08-28 19:47:23 -0600

    fixed typo

--- db/GET_SEASONS.sql
+++ db/GET_SEASONS.sql
@@ -4,5 +4,5 @@ ON seasons.league_id = leagues.league_id
 JOIN users
 ON leagues.user_id = users.user_id
 WHERE ($1 IS NULL OR seasons.league_id=$1)
-AND ($2 IS NULL OR season.season_id=$2)
+AND ($2 IS NULL OR seasons.season_id=$2)
 AND (users.user_id=$3);
