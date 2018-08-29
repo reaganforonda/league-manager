2c993dbb9ae97e0556f718229364e71ec173221d
reaganforonda <foronda.reagan@gmail.com>
2018-08-28 18:57:53 -0600

    create sql for getting seasons

---
+++ db/GET_SEASONS.sql
@@ -0,0 +1,8 @@
+SELECT * FROM SEASONS
+JOIN leagues
+ON seasons.league_id = leagues.league_id
+JOIN users
+ON leagues.user_id = users.user_id
+WHERE ($1 IS NULL OR seasons.league_id=$1)
+AND ($2 IS NULL OR season.season_id=$2)
+AND (users.user_id=$3);
