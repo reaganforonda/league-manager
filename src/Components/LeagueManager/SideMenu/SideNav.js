36a94aca5b0c5cd8a99c00acee79b3ab7d399a3f
reaganforonda <foronda.reagan@gmail.com>
2018-08-30 15:38:10 -0600

    styling

--- src/Components/LeagueManager/SideMenu/SideNav.js
+++ src/Components/LeagueManager/SideMenu/SideNav.js
@@ -5,13 +5,27 @@ export default function SideNave(props){
     return (
         <div className='league-side-nav'>
             <div className='league-side-nav-menu'>
-                <Link to='/leaguemanager/dashboard'>Dashboard</Link>
-                <Link to='/leaguemanager/leagueview'>Leagues</Link>
-                <Link to='/leaguemanager/seasons'>Seasons</Link>
-                <Link to='/leaguemanager/fixtures'>Fixtures</Link>
-                <Link to='/leaguemanager/teams'>Teams</Link>
-                <Link to='/leaguemanager/players'>Players</Link>
-                <Link to='/leaguemanager/stadiums'>Stadiums</Link>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/dashboard'>Dashboard</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/leagueview'>Leagues</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/seasons'>Seasons</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/fixtures'>Fixtures</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/teams'>Teams</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/players'>Players</Link>
+                </div>
+                <div className='league-side-nav-menu-item'>
+                    <Link to='/leaguemanager/stadiums'>Stadiums</Link>
+                </div>
             </div>  
         </div>
     )
