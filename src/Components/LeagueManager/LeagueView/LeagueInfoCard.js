7791b1af88f5397c1ec1274fba3c5e5f93d45bad
reaganforonda <foronda.reagan@gmail.com>
2018-08-30 12:10:46 -0600

    deleted unused file

--- src/Components/LeagueManager/LeagueView/LeagueInfoCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LeagueInfoCard(props){
-    return (
-        <div className='league-info-card-container'>
-            League Name
-        </div>
-    )
-}
