2a2ffe047b6e50ce2860f1a4df71a210a3cb469f
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 23:36:36 -0600

    add getfixtures

--- src/ducks/reducers/fixtureReducer.js
+++ src/ducks/reducers/fixtureReducer.js
@@ -6,13 +6,25 @@ const INITIAL_STATE = {
 
 const GET_ALL_FIXTURES = 'GET_ALL_FIXTURES';
 
-function getAllFixtures(userID) {}
+export function getFixtures(userID) {
+    let fixtures = axios.get(``).then((result) => {
+        return result.data
+    })
 
-
+    return {
+        type: GET_ALL_FIXTURES,
+        payload: fixtures
+    }
+}
 
 export default function fixtureReducer(state = INITIAL_STATE, action){
     switch(action.type){
 
+        case GET_ALL_FIXTURES + "_PENDING":
+            return "Loading";
+        case GET_ALL_FIXTURES + "_FULFILLED":
+            return Object.assign({}, state, {allFixtures: action.payload})
+        
         default:
             return state;
     }
