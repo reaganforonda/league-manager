bff1674b014916e8214b673a4fda7ace0b2cbd5f
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 23:15:53 -0600

    create dropdown for season

---
+++ src/Components/DropdownMenus/SeasonDropDown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as genUtil from '../../Utilities/generalUtil'
+
+export default function SeasonDropDown(props) {
+
+    
+    let seasons = props.seasons.map((season, index) => {
+        if(~~props.league === ~~season.league_id) {
+            return(
+                <option value={season.season_id} key={season.season_id + index}>{genUtil.truncateDate(season.season_start_date)} - {genUtil.truncateDate(season.season_end_date)}</option>
+            )
+        }
+    })
+
+    return (
+        <select onChange={(e)=> props.selectSeason(e)} name='seasonID' className='league-season-drop-down'>
+            <option value='' disabled selected>Select Season</option>
+            {seasons}
+        </select>
+    )
+}
