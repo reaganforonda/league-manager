3a267c7edf183305e3e2a7f2495ad01bd1dc4dee
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 20:38:57 -0600

    create dropdown for stadiums

---
+++ src/Components/DropdownMenus/StadiumDropDown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function StadiumDropDown(props) {
+    let stadiums = props.stadiums.map((stadium, index) => {
+        return <option key={stadium.stadium_id+stadium.stadium_name + index}>{stadium.stadium_name}</option>
+    })
+
+    return (
+        <select onChange={(e) => props.selectStadium(e)} name='stadiumID' className='league-stadium-drop-down'>
+            <option value='' disabled selected>Select Stadium</option>
+            {stadiums}
+        </select>
+    )
+}
