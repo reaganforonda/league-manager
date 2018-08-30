01686f220588e90bb153d2421693e9c7deeaace7
reaganforonda <foronda.reagan@gmail.com>
2018-08-29 23:16:15 -0600

    fix create fixture

--- server/controllers/fixtureController.js
+++ server/controllers/fixtureController.js
@@ -4,15 +4,15 @@ module.exports ={
     addFixture: (req, res) => {
         const db = req.app.get('db');
         const {
-            fixture_date,
-            league_id,
-            season_id,
-            stadium_id,
-            home_team,
-            away_team
+            fixtureDate,
+            leagueID,
+            seasonID,
+            stadiumID,
+            homeTeamID,
+            awayTeamID
         } = req.body;
 
-        db.CREATE_FIXTURE([fixture_date, league_id, season_id, stadium_id, home_team, away_team]).then((result) => {
+        db.CREATE_FIXTURE([fixtureDate, leagueID, seasonID, stadiumID, homeTeamID, awayTeamID]).then((result) => {
             res.status(200).send(result);
         }).catch((err) => {
             console.log(`Server error while attempting to create new fixture: ${err}`);
