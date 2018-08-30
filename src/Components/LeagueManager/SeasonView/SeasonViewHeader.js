5674f01276fd63ca0465708b99c0608483002097
reaganforonda <foronda.reagan@gmail.com>
2018-08-30 17:07:35 -0600

    styling

--- src/Components/LeagueManager/SeasonView/SeasonViewHeader.js
+++ src/Components/LeagueManager/SeasonView/SeasonViewHeader.js
@@ -3,10 +3,14 @@ import {Link} from 'react-router-dom';
 
 export default function SeasonViewHeader(props) {
     return (
-        <header className='season-view-header'>
-            <div className='season-view-menu'>
-                <Link to='/leaguemanager/seasons'>Main</Link>
-                <Link to='/leaguemanager/seasons/add'>Add</Link>
+        <header className='view-header'>
+            <div className='view-header-menu'>
+                <div className='view-header-menu-item'>
+                    <Link to='/leaguemanager/seasons'>Main</Link>
+                </div>
+                <div className='view-header-menu-item'>
+                    <Link to='/leaguemanager/seasons/add'>Add</Link>
+                </div>
             </div>
         </header>
     )
