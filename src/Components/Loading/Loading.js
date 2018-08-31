f17930ef58e65328c104e1614596a38d852e6eba
reaganforonda <foronda.reagan@gmail.com>
2018-08-31 16:07:27 -0600

    styling

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 export default function Loading(){
     return (
-        <div className='loading'>
-            <h1>LOADING</h1>
+        <div className='view-container'>
+            <div className='loading'>
+                <h1>LOADING</h1>
+                <div className='loader'>
+                </div>
+            </div>
+            
         </div>
         
     )
