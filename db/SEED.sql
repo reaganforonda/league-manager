0b576c271c24db62322cdfd5b7d2d9bc16f56b75
reaganforonda <foronda.reagan@gmail.com>
2018-09-02 14:11:02 -0600

    added teams to seed file

--- db/SEED.sql
+++ db/SEED.sql
@@ -11,4 +11,105 @@ VALUES
 INSERT INTO account_types
 (acct_name)
 VALUES
-('Player')
+('Player');
+
+-- TEAMS
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Liverpool', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Chelsea', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Watford', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Manchester City', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Tottenham', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Bournemouth', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Everton', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Leicester City', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Arsenal', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Manchester United', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Wolverhampton Wolves', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Southampton', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Fulham', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Brighton', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Crystal Palace', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Cardiff City', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Huddersfield', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Newcastle', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'Burnley FC', 'Denver', 'CO', 80202);
+
+INSERT INTO teams
+(league_id, league_manager, team_name, team_city, team_state, team_zip)
+VALUES
+(1, 1, 'West Ham', 'Denver', 'CO', 80202);
