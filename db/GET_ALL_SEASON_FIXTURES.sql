2ff6c4f125f7b361a15cde76a2a2a7b0d66ca0a0
reaganforonda <foronda.reagan@gmail.com>
2018-09-02 20:55:35 -0600

    updated sql file

--- db/GET_ALL_SEASON_FIXTURES.sql
+++ db/GET_ALL_SEASON_FIXTURES.sql
@@ -1,4 +1,6 @@
 SELECT *
 FROM fixtures
-WHERE ($1 is null OR league_id = $1)
+JOIN leagues
+ON leagues.league_id = fixtures.league_id
+WHERE ($1 is null OR fixtures.league_id = $1)
 AND ($2 is null OR season_id = $2)   
